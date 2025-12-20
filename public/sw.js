38cc630bf7d303c0afb0218b94ef653742efa73d
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-12-19 16:17:35 -0800

    chore: format

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,22 @@
 // stolen from https://developer.chrome.com/docs/workbox/caching-strategies-overview#cache_first_falling_back_to_network
 
-const cacheName = 'MyFancyCacheName_v1';
+const cacheName = "MyFancyCacheName_v1";
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.destination === 'image') {
-    event.respondWith(caches.open(cacheName).then((cache) => {
-      return cache.match(event.request.url).then((cachedResponse) => {
-        if (cachedResponse) 
-          return cachedResponse;
+self.addEventListener("fetch", (event) => {
+  if (event.request.destination === "image") {
+    event.respondWith(
+      caches.open(cacheName).then((cache) => {
+        return cache.match(event.request.url).then((cachedResponse) => {
+          if (cachedResponse) return cachedResponse;
 
-        return fetch(event.request).then((fetchedResponse) => {
-          cache.put(event.request, fetchedResponse.clone());
+          return fetch(event.request).then((fetchedResponse) => {
+            cache.put(event.request, fetchedResponse.clone());
 
-          return fetchedResponse;
+            return fetchedResponse;
+          });
         });
-      });
-    }));
+      }),
+    );
   } else {
     return;
   }
