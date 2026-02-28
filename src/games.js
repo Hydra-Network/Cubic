da343672d666117de76f33cd181d1247b03a2160
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2026-02-27 17:10:49 -0800

    feat: update for new changes in hydra-assets

--- src/games.js
+++ src/games.js
@@ -57,13 +57,13 @@ function updateSidebar() {
 function loadFrame(data) {
 	const { frame, file_name, frameGame } = data;
 	if (frameGame == "true") {
-		frame.src = `https://raw.githack.com/hydra-network/hydra-assets/main/${file_name}`;
+		frame.src = `https://raw.githack.com/hydra-network/hydra-assets/main/gmes/${file_name}`;
 	} else {
 		frame.onload = async function() {
 			if (this.dataset.loaded) return;
 			const doc = this.contentDocument;
 			const html = await fetch(
-				`https://cdn.jsdelivr.net/gh/hydra-network/hydra-assets@main/${file_name}`,
+				`https://cdn.jsdelivr.net/gh/hydra-network/hydra-assets@main/gmes/${file_name}`,
 			).then((r) => r.text());
 
 			doc.open();
